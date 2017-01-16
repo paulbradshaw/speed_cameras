24230ec6cc55c7c19d5036f759a093f907edaa0c
Paul Bradshaw <paulonhismobile@gmail.com>
2017-01-16 20:49:22 +0000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,24 +1,14 @@
-# This is a template for a Python scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+#import our libraries
+import scraperwiki
+import urllib2
+import lxml.etree
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
+#create a variable called 'url' and then read what's there
+url = "http://www.staffssaferroads.co.uk/media/114997/03092012_forwebsite.pdf"
+pdfdata = urllib2.urlopen(url).read()
+print "The pdf file has %d bytes" % len(pdfdata)
 
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+#convert to xml and print some info
+xmldata = scraperwiki.pdftoxml(pdfdata)
+print "After converting to xml it has %d bytes" % len(xmldata)
+root = lxml.etree.fromstring(xmldata)
